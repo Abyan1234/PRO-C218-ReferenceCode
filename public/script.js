c1c57cb5031dbf7047441f32d29442b12d73e29e
Abyan1234 <70338733+Abyan1234@users.noreply.github.com>
2023-03-30 19:14:17 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -99,6 +99,23 @@ $(function () {
         }
         
     })
+    $("#invite_button").click(function(){
+        const to=prompt("enter the email address")
+        let data={
+            url:window.location.href,
+            to:to,
+
+        }
+        $.ajax({
+            url:"/send-mail",
+            type:"post",
+            data:JSON.stringify(data),
+            dataType:'json',
+            contentType:'application/json',
+            success:function(result){alert("invite sent")},
+            error:function(result){alert("invite failed to send")},
+        })
+    })
 })
 
 peer.on("open",(id)=>{
